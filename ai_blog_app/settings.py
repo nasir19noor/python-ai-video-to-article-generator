9f86dddfd3bcceb505217aea3800305fea4cf79f
nasir19noor <nasir@nasir.id>
2024-12-26 00:45:28 +0700

    update

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ai-video-to-article-converter',
+        'USER': 'postgres',
+        'PASSWORD': '@Kun4$!r',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
