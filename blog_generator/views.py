7c1db4cefaeb17067efab87b2bab802c642cbbf1
nasir19noor <nasir@nasir.id>
2024-12-26 23:28:27 +0700

    update

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,13 +1,50 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
 
 # Create your views here.
-
+@login_required
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
+def generate_blog(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            yt_link = data  ['link']
+            return JsonResponse({'content': yt_link})
+        except (KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'Invalid data sent'}, status=400)
+        
+        # get the video title
+
+        # get the video transcript
+
+        #Use openAI to generate the blog
+
+        #Save the blog to the database
+
+        #return the blog article as a response
+
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid credentials'
+            return render(request, 'login.html', {'error_message': error_message})
     return render(request, 'login.html')
 
 def user_signup(request):
@@ -32,4 +69,5 @@ def user_signup(request):
     return render(request, 'signup.html')
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/')
